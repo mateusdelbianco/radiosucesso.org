0668cba49e094f9e333892128d6ad9dd93d1bb6a
Mateus Del Bianco <mateus@delbianco.net.br>
2013-01-17 23:48:16 -0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Radiosucesso::Application.config.secret_token = 'e86f35c9f6fdfa8b4e1f4b912f44cdf42a17e7ec3629336200ec529ca3f9b7da30f0b497c24b1a317633d8e4e2dcc65065e438145473864cafc827ccc731d306'
