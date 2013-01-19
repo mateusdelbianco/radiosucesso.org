ded8046f496c878c30ffce155fc83ad8d507fc99
Mateus Del Bianco <mateus@delbianco.net.br>
2013-01-19 00:27:02 -0200

    Youtube Player

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,4 +5,12 @@ class WelcomeController < ApplicationController
     @page = PostFetcher.page
     @posts = Post.where(:type => 'video').order_by(:created_time.desc)
   end
+
+  def next_id
+    post = Post.only(:link, :name).sample
+    render json: {
+      title: post.name,
+      id: CGI.parse(URI.parse(post.link).query)['v'].first
+    }
+  end
 end
