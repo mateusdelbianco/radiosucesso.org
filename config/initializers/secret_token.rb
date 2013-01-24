036d8714fb398e7f04ee6101e517cb02d7358e36
Mateus Del Bianco <mateus@delbianco.net.br>
2013-01-24 14:43:15 -0200

    prepares the code to public release

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Radiosucesso::Application.config.secret_token = 'b74feb11e2ed4f9416fd4b6a28f236a8c462b222cc241b8458532312344ed966f7828631e9f51dd6925fc27a3a905d6717ec14fe062d78a23e990a34d297a184'
+Radiosucesso::Application.config.secret_token = ENV['SECRET_TOKEN']
